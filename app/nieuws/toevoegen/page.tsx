9f684befd94d8e2e2a32ffefce21b80804c565d3
Iwan Lemmens <github.playmate094@passmail.net>
2024-09-20 17:49:10 +0200

    🔒️ secure route

--- app/nieuws/toevoegen/page.tsx
+++ app/nieuws/toevoegen/page.tsx
@@ -1,7 +1,14 @@
 import NewsUpdateForm from "@/components/news/NewsUpdateForm";
+import AccessDenied from "@/components/user/AccessDenied";
+import { getUserIdWithAccess } from "@/lib/NextAuthFunctions";
 
-const NewsAdd = () => {
-  return <NewsUpdateForm />;
+const NewsAdd = async () => {
+  const userIdWithAccess = await getUserIdWithAccess(["ADMIN", "WRITER"]);
+  if (userIdWithAccess) {
+    return <NewsUpdateForm />;
+  } else {
+    return <AccessDenied />;
+  }
 };
 
 export default NewsAdd;
