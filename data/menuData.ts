7901cf59d5d7aea6d480d7c9c676f45d215a5264
Iwan Lemmens <github.playmate094@passmail.net>
2024-12-02 14:03:05 +0100

    🗃️ add fields for calendar

--- data/menuData.ts
+++ data/menuData.ts
@@ -3,35 +3,35 @@ import type { MenuItemProps } from "@/lib/dataTypes";
 export const menuData: MenuItemProps[] = [
   {
     label: "Over ons",
-    href: "/about",
+    href: "/over-ons",
     children: [
       {
         label: "Foto's",
         info: "Bekijk een verzameling prachtige beelden die de energie en passie van onze Ierse dansoptredens en evenementen vastleggen.",
-        href: "/about/photos",
+        href: "/onver-ons/fotos",
       },
       {
         label: "Video's",
         info: "Ontdek onze inspirerende video's die de dynamiek en sfeer van onze Ierse dansvoorstellingen en workshops weergeven.",
-        href: "/about/videos",
+        href: "/onver-ons/videos",
       },
       {
         label: "Historie",
         info: "Een dynamische vereniging die de Ierse cultuur promoot in België door middel van danslessen, optredens en workshops, opgericht in 2008",
-        href: "/about/history",
+        href: "/onver-ons/historie",
       },
       {
         label: "Ierse Dans",
         info: "De wereld ontdekte Ierse dans in 1994 tijdens het Eurovisiesongfestival, wat leidde tot het fenomenale succes van Riverdance en Lord of the Dance, die de traditionele Ierse dans wereldwijd populair maakten.",
-        href: "/about/irish-dance",
+        href: "/onver-ons/ierse-dans",
       },
       {
         label: "Ons Team",
         info: "Maak kennis met ons gemotiveerde team van dansers en instructeurs die zich inzetten om de Ierse cultuur te delen en Ierse dans naar een breder publiek te brengen.",
-        href: "/about/team",
+        href: "/onver-ons/team",
       },
     ],
   },
-  { label: "Danslessen", href: "/news" },
-  { label: "Kalender", href: "/news" },
+  { label: "Danslessen", href: "/nieuws" },
+  { label: "Kalender", href: "/kalender" },
 ];
