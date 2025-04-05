29e0743663e0cdc3f8a59f3f07706ad798894ba4
Iwan Lemmens <github.playmate094@passmail.net>
2025-04-05 12:45:54 +0000

    🚧 changing auth to Logto

---
+++ lib/logto.ts
@@ -0,0 +1,8 @@
+export const logtoConfig = {
+    endpoint: 'https://cfb8d8.logto.app/',
+    appId: 'bf1osk635o2oowzxrru9x',
+    appSecret: 'iRkP4603sx644ERiovOQTyFNLKhiBNn2',
+    baseUrl: 'https://cautious-capybara-575g44jwxg72565-3000.app.github.dev', // Change to your own base URL
+    cookieSecret: 'UpE1GuqYKxoIMsZVQUt48OvCvycuYYDu', // Auto-generated 32 digit secret
+    cookieSecure: process.env.NODE_ENV === 'production',
+  };
