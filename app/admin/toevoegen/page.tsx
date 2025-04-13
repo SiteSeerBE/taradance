7f2b28b08c085aeea16e93a5aa72452c2772c240
Iwan Lemmens <github.playmate094@passmail.net>
2025-04-13 22:47:19 +0200

    🐛 remove echo's of security

--- app/admin/toevoegen/page.tsx
+++ app/admin/toevoegen/page.tsx
@@ -1,14 +1,7 @@
 import NewsUpdateForm from "@/components/news/NewsUpdateForm";
-import AccessDenied from "@/components/user/AccessDenied";
-import { getUserIdWithAccess } from "@/lib/NextAuthFunctions";
 
 const NewsAdd = async () => {
-  const userIdWithAccess = await getUserIdWithAccess(["ADMIN", "WRITER"]);
-  if (userIdWithAccess) {
-    return <NewsUpdateForm />;
-  } else {
-    return <AccessDenied />;
-  }
+  return <NewsUpdateForm />;
 };
 
 export default NewsAdd;
