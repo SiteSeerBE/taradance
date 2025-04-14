18b4e273187f3b25cc1b6db3062042e22931700c
Iwan Lemmens <github.playmate094@passmail.net>
2025-04-14 13:43:46 +0200

    🧑‍💻 adding baseUrl to env

--- lib/logto.ts
+++ lib/logto.ts
@@ -2,7 +2,7 @@ export const logtoConfig = {
   endpoint: "https://pohjzp.logto.app/",
   appId: process.env.LOGTO_APP_ID!,
   appSecret: process.env.LOGTO_APP_SECRET!,
-  baseUrl: "https://taradance.vercel.app/", // Change to your own base URL
+  baseUrl: process.env.LOGTO_BASE_URL!,
   cookieSecret: process.env.LOGTO_COOKIE_SECRET!, // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === "production",
 };
