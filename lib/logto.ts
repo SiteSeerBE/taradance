b1fce531a71f4380c11080af4f1d5f0f511c85a7
Iwan Lemmens <github.playmate094@passmail.net>
2025-04-14 12:54:34 +0200

    🔒️ change base url for logto

--- lib/logto.ts
+++ lib/logto.ts
@@ -4,7 +4,7 @@ export const logtoConfig = {
   endpoint: "https://pohjzp.logto.app/",
   appId: process.env.LOGTO_APP_ID!,
   appSecret: process.env.LOGTO_APP_SECRET!,
-  baseUrl: "http://localhost:3000", // Change to your own base URL
+  baseUrl: "https://pohjzp.logto.app/", // Change to your own base URL
   cookieSecret: process.env.LOGTO_COOKIE_SECRET!, // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === "production",
 };
