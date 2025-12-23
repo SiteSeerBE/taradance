8dfdf37e4176662922213b3ff3b660eb7b749dc3
Iwan Lemmens <github.playmate094@passmail.net>
2025-12-23 10:27:03 +0100

    🐛 fix pages links in admin

---
+++ app/inhoud/page.tsx
@@ -0,0 +1,22 @@
+import DropdownContentNoJs from "@/components/menu/DropdownContentNoJs";
+import { prisma } from "@/lib/prisma";
+import type { Menu } from "@prisma/client";
+
+const MENU_PARENT_ID = 4;
+
+const menuData = await prisma.menu.findMany({
+  where: { parentId: MENU_PARENT_ID },
+  orderBy: [{ orderId: "asc" }, { id: "asc" }],
+  select: {
+    id: true,
+    contentPath: true,
+    description: true,
+    title: true,
+  },
+});
+
+const OverOnsPage = () => {
+  return <DropdownContentNoJs submenuscontent={menuData} />;
+};
+
+export default OverOnsPage;
---
+++ app/inhoud/videos/page.tsx
@@ -0,0 +1,9 @@
+const Videos: React.FC = () => {
+  return (
+    <div>
+      <h1>Videos</h1>
+    </div>
+  );
+};
+
+export default Videos;
