e4171891cf748613c6f7d17e18acf7e72aadf436
Iwan Lemmens <github.playmate094@passmail.net>
2026-07-25 10:35:32 +0200

    🔥remove breadcrumb

--- app/over-ons/fotos/page.tsx
+++ app/over-ons/fotos/page.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { Breadcrumb, Breadcrumbs } from "@/components/breadcrumbs";
-
 const Fotos: React.FC = async () => {
     return (
     <>
       <header className="container mt1">
         <hgroup>
           <h1>Foto&apos;s</h1>
-          <Breadcrumbs>
-            <Breadcrumb href="/">Taradance</Breadcrumb>
-            <Breadcrumb href="/over-ons">Over Ons</Breadcrumb>
-            <Breadcrumb>Foto&apos;s</Breadcrumb>
-          </Breadcrumbs>
+          <hr />
         </hgroup>
       </header>
       <embed id="iframe" src="https://widgets.sociablekit.com/flickr-albums/iframe/25682959" width="100%" height="600" title="Flickr Albums" />
